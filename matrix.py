58d0e30f4d59729b257bdce60a65744b14a02da2
Jaime Melis <jmelis@opennebula.org>
2012-09-05 23:03:10 +0200

    refactors the Matrix class: - splits non mathematical specific functionality to another class: MinCombMatrix - replace class name hardcoding with dynamic class name - extracts the sorted_diagonal functionality of the find_pair to its own method

--- matrix.py
+++ matrix.py
@@ -17,70 +17,27 @@ class Matrix:
 
     @classmethod
     def build(cls,lst):
-        m = Matrix()
+        m = cls()
         m.rows = lst
         m.setdim()
         return m
 
     @classmethod
     def identity(cls,nr):
-        m = Matrix(nr)
+        m = cls(nr)
         for i in range(nr):
             m[i,i] = 1
         return m
-
-    # print Matrix.s([0,1,2],5)
-    # print Matrix.s([[0,1,2],[3]],5)
-
-    @classmethod
-    def d(cls,sets,nr=None):
-        if not nr:
-            nr = len(sets)
-        d = Matrix(nr)
-        for set in sets:
-            s = Matrix(nr)
-            for i in xrange(nr):
-                for j in xrange(nr):
-                    if i in set and j in set:
-                        s[i,j] = 1
-            d = d + s
-        return d
-
-    def find_pair(self,r):
-        m = Matrix(self.nr,self.nr+1)
-        m = m + self
-
-        for index,row in enumerate(m.rows):
-            row[-1] = index
-
-        sorted_diagonal = sorted(enumerate(self.diagonal()),key=lambda e: e[1])
-        indexes = [e[0] for e in sorted_diagonal]
-
-
-        sorted_m_rows = []
-        for index in indexes:
-             sorted_m_rows.append(self.rows[index])
-
-        sorted_m = Matrix.build(sorted_m_rows)
-
-        next_j = False
-        for i in r[-1][::-1]:
-            col = sorted_m.col(i)
-            if 0 in col:
-                next_j = indexes[col.index(0)]
-                break
-
-        return next_j
 
     def setdim(self):
         self.nr = len(self.rows)
         self.nc = len(self.rows[0])
 
     def transpose(self):
-        return Matrix.build(zip(*self.rows))
+        return self.__class__.build(zip(*self.rows))
 
     def __add__(self,other):
-        m = Matrix(self.nr,self.nc)
+        m = self.__class__(self.nr,self.nc)
 
         for i in xrange(self.nr):
             for j in xrange(self.nc):
@@ -89,7 +46,7 @@ class Matrix:
         return m
 
     def __sub__(self,other):
-        m = Matrix(self.nr,self.nc)
+        m = self.__class__(self.nr,self.nc)
 
         for i in xrange(self.nr):
             for j in xrange(self.nc):
@@ -98,7 +55,7 @@ class Matrix:
         return m
 
     def __mul__(self,other):
-        m = Matrix(self.nr,other.nc)
+        m = self.__class__(self.nr,other.nc)
         for i in range(self.nr):
             for j in range(other.nc):
                 for k in range(other.nr):
@@ -143,12 +100,12 @@ class Matrix:
         return [row[j] for row in self.rows]
 
     def swap_row(self,i, j):
-        m = Matrix.build(self.rows)
+        m = self.__class__.build(self.rows)
         m.rows[i],m.rows[j] = self.rows[j],self.rows[i]
         return m
 
     def swap_col(self,i,j):
-        m = Matrix.build(self.rows)
+        m = self.__class__.build(self.rows)
         m = m.transpose()
         m = m.swap_row(i,j)
         return m.transpose()
@@ -166,8 +123,68 @@ class Matrix:
                 diagonal_list.append(self[i-offset,i])
         return diagonal_list
 
+    def clone(self):
+        return self.build(self.rows)
+
+class MinCombMatrix(Matrix):
+    @classmethod
+    def pairs(cls,sets,nr=None):
+        if not nr:
+            nr = len(sets)
+        d = cls(nr)
+        for set in sets:
+            s = cls(nr)
+            for i in xrange(nr):
+                for j in xrange(nr):
+                    if i in set and j in set:
+                        s[i,j] = 1
+            d = d + s
+        return d
+
+    # def sort_diagonal(self):
+
+    def sorted_diagonal(self):
+        m = self.__class__(self.nr,self.nc+1)
+        m = m + self
+
+        # add row index to last column
+        for i,row in enumerate(m.rows):
+            row[-1] = i
+
+        # get array with the sorted positions of the diagonal
+        sorted_diagonal = sorted(enumerate(self.diagonal()),key=lambda e: e[1])
+        indexes = [e[0] for e in sorted_diagonal]
+
+        # reorder by the indexes array
+        sorted_m_rows = []
+        for index in indexes:
+             sorted_m_rows.append(m.rows[index])
+
+        # return a new Matrix object with the sorted rows in place
+        sorted_m = self.__class__.build(sorted_m_rows)
+        return sorted_m
+
+    def find_pair(self,r):
+        sorted_m = self.sorted_diagonal()
+        indexes = sorted_m.transpose().rows[-1]
+
+        next_j = False
+
+        for i in r[-1][::-1]:
+            col = sorted_m.col(i)
+            if 0 in col:
+                next_j = indexes[col.index(0)]
+                break
+
+        return next_j
+
+    def find_next(self):
+        pass
+        # obtener la fila con mayor numero de 0s de la matriz sorted_diagonal
+
+
 if __name__ == "__main__":
-    r = r = [[0,1,3],[1,4,0],[3,2]]
-    m = Matrix.d(r,5)
+    r = [[0,1,3],[1,4,0],[3,2]]
+    m = MinCombMatrix.pairs(r,5)
     print m.find_pair(r)
 
