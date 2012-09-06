0cc456a0d18ca7a96904379f11c2a84bb88d38e3
Jaime Melis <jmelis@opennebula.org>
2012-09-06 12:34:06 +0200

    Fix list problem.

--- min_times_k_algor.py
+++ min_times_k_algor.py
@@ -22,10 +22,7 @@ class Results:
         return len(self.column) == self.K
 
     def r(self):
-        if self.subsets:
-            r = self.subsets
-        else:
-            r = []
+        r = self.subsets[:]
         if self.column:
             r.append(self.column)
         return r
@@ -60,8 +57,8 @@ class MinTimesK:
         self.N = n
         self.K = k
 
-        self.i = None
-        self.j = None
+        self.i = 0
+        self.j = 0
 
         self.results          = Results(self.N,self.K)
         #self.adjacency_matrix = M(self.N)
@@ -72,51 +69,51 @@ class MinTimesK:
     def debug(self,label):
         i = self.i
         j = self.j
-
         print "[%s] i=%s, j=%s" % (label,str(i),str(j))
 
     def solve(self):
-        self.i = self.find_next()
-        self.results.append(self.i)
+        self.assign_i(self.find_next())
 
-        # while not self.finished():
-        for kk in range(4):
+        while True:
+            print "\n--- loop %i ---" % (self.safe_exit,)
+            self.assign_j(self.find_pair())
+            if self.finished():
+                break
 
-            # print "--- %i" % (kk)
-            # self.debug("uno")
-
-            self.j = self.find_pair()
-            self.results.append(self.j)
-
-            # self.debug("dos")
-
-            if self.j:
-                # self.debug("tres")
-                self.i = self.j
+            if self.j != None:
+                self.j_to_i()
             else:
-                self.i = self.find_next()
-                self.results.append(self.i)
+                self.assign_i(self.find_next())
+                if self.finished():
+                    break
 
+            print self.results.r()
 
-            # self.debug("cuatro")
-
-        print "---"
+        print "--- END ---"
         print "r      ",self.results.r()
         print "column ",self.results.column
         print "subsets",self.results.subsets
 
-        # wtf
-        self.results.subsets[1][0] = 7
-        print "subsets",self.results.subsets
+    def j_to_i(self):
+        # print "i=j=%i" % (self.j)
+        j = self.j
+        self.i = j
+
+    def assign_i(self,i):
+        # print "assign_i: %s" % (str(i),)
+        self.i = i
+        self.results.append(i)
+
+    def assign_j(self,j):
+        # print "assign_j: %s" % (str(j),)
+        self.j = j
+        self.results.append(j)
 
     def adjacency_matrix(self):
         return M.pairs(self.results.r(),self.N)
 
     def find_next(self):
-        if not self.i:
-            return 0
-        else:
-            return self.adjacency_matrix().find_next()
+        return self.adjacency_matrix().find_next()
 
     def find_pair(self):
         return self.adjacency_matrix().find_pair(self.results.r())
@@ -127,11 +124,11 @@ class MinTimesK:
         col_complete = self.results.column_complete()
         all_pairs    = self.adjacency_matrix().all_pairs()
 
-        if self.safe_exit > 10 or (col_complete and all_pairs):
-            print self.results.subsets
+        if self.safe_exit > 20 or (col_complete and all_pairs):
             return True
         else:
             return False
+
 
 min_t_k = MinTimesK(5,3)
 min_t_k.solve()
